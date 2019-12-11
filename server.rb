a28600175a832115449a8f096f6e9374c86c076c
Sam <homeboy@Sams-MacBook-Pro.local>
2019-12-11 00:58:21 -0500

    created albums page and put data in external file

--- server.rb
+++ server.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'pry'
+require './assets/image_assets'
+
+include ImageAssets
 
 get '/' do
+  @images = ImageAssets.hash["images"]
   erb :index
+end
+
+get '/music/:album' do
+  @album = ImageAssets.hash["albums"]
+  erb :albums
 end
