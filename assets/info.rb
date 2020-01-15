b9144dfe762d6fe601709dca1b383f91780c5730
Sam <homeboy@Sams-MacBook-Pro.local>
2020-01-14 22:19:07 -0500

    album page

--- assets/info.rb
+++ assets/info.rb
@@ -21,6 +21,7 @@ module Info
         "name" => "",
         "desc" => "",
       },
+<<<<<<< Updated upstream
       "two" => {
         "link" => "",
         "name" => "",
@@ -30,6 +31,24 @@ module Info
         "link" => "",
         "name" => "",
         "desc" => "",
+=======
+
+      "albums" => {
+        "crash" => {
+          "title" => "i crashed my spaceship",
+          "description" => "",
+          "date" => "12/12/19",
+          "cover" => "https://i.postimg.cc/dQ8mwgk9/i-crashed-my-spaceship.png",
+          "bandcamp" => "1987238980"
+        },
+        "sometime" => {
+          "title" => "Sometime Soon",
+          "description" => "My first \"official\" album.",
+          "date" => "1/12/18",
+          "cover" => "https://i.postimg.cc/mghfz154/cover.png",
+          "bandcamp" => "3914218437",
+        }
+>>>>>>> Stashed changes
       }
     },
 
