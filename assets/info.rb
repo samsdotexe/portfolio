4af15663cc5c96451a5bb7a09294fa377898f849
samsdotexe <50642184+samsdotexe@users.noreply.github.com>
2020-01-29 22:35:11 -0500

    Update info.rb

--- assets/info.rb
+++ assets/info.rb
@@ -26,38 +26,38 @@ module Info
         "one" => {
           "link" => "https://i.postimg.cc/2yWDdPPC/turkey-skeleton.png",
           "name" => "Turkey Skeleton",
-          "desc" => "Drawn on paper with a fineliner pen and a water color brush with a well of ink. Scanned in and touched up with Photoshop."
+          "desc" => "Drawn on paper with a fineliner pen, a watercolor brush, and a well of ink. Scanned in and touched up with Photoshop."
         },
         "two" => {
           "link" => "https://i.postimg.cc/BnvsVsQN/the-boys.png",
           "name" => "The Boys",
-          "desc" => "(put description here)"
+          "desc" => "A panel from a work in progress webcomic based on the YouTube group SuperMega. "
         },
         "three" => {
           "link" => "https://i.postimg.cc/FKmb4P8P/bedroom.png",
           "name" => "Bedroom",
-          "desc" => "(put description here)"
+          "desc" => "Created a rough 3D bedroom model in Blender for reference; traced and colored in Clip Studio Paint."
         },
         "four" => {
           "link" => "https://i.postimg.cc/PfSby4qJ/cheer-down.png",
           "name" => "Cheer Down",
-          "desc" => "(put description here)"
+          "desc" => "Inspired by a George Harrison song of the same name, this low resolution Photoshop drawing reflects the dark comedic story within the song."
         },
         "five" => {
           "link" => "https://i.postimg.cc/3RqmtDLb/bugs.png",
           "name" => "Bugs",
-          "desc" => "(put description here)"
+          "desc" => "An interest in drawing the anatomy of insects led to this sketch. Painted in Photoshop."
         },
         "six" => {
           "link" => "https://i.postimg.cc/P5BqVQhD/gorl.jpg",
           "name" => "Girl",
-          "desc" => "(put description here)"
+          "desc" => "A character concept sketch done with a blue graphite mechanical pencil on sketchbook paper."
         }
       },
       "albums" => {
         "crash" => {
           "title" => "i crashed my spaceship",
-          "description" => "",
+          "description" => "My second original composition.",
           "date" => "12/12/19",
           "cover" => "https://i.postimg.cc/dQ8mwgk9/i-crashed-my-spaceship.png",
           "soundcloud" => "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/915352924&color=%23050505&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
